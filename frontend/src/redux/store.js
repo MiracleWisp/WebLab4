5095524bfd9a4646be77b69dde7ee8a97bded081
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-12-17 00:07:26 +0300

    REACT: fix reloading Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,33 @@
 import {createStore} from "redux";
 import rootReducer from "./reducers"
+import axios from "axios";
 
+async function getInitState() {
+    let state = {
+        authReducer: {
+            username: '',
+            isAuthenticated: false
+        }
+    };
+    const result = await axios({
+        method: 'get',
+        url: 'http://localhost:8080/api/roles',
+        withCredentials: true
+    });
+    console.log(result.status);
+    if (result.status === 200) {
+        state.authReducer.username = result.data.username;
+        state.authReducer.isAuthenticated = result.data.successful;
+    }
+    return createStore(rootReducer, state, window.__REDUX_DEVTOOLS_EXTENSION__
+        && window.__REDUX_DEVTOOLS_EXTENSION__())
+}
+
+export default getInitState();
+
+/*
 export default createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+)*/
