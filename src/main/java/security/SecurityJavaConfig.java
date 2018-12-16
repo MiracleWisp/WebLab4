4a469e8eba9a64ba1bc81d933ec596b8376a59ff
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-12-16 23:51:57 +0300

    SPRING: add logout Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/main/java/security/SecurityJavaConfig.java
+++ src/main/java/security/SecurityJavaConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.util.Arrays;
 
@@ -54,7 +55,13 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(successHandler)
                 .failureHandler(failureHandler)
                 .and()
-                .logout();
+                .logout()
+                .deleteCookies("remove")
+                .logoutUrl("/api/logout")
+                .permitAll()
+                .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                });
     }
 
 
